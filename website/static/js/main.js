18af677af9c8183ea8317a1c8852f368631d5138
michelrivas <M.Rivas1@uni.brighton.ac.uk>
2015-11-06 15:31:43 +0000

    Adding bootstrap

---
+++ website/static/js/main.js
@@ -0,0 +1,9 @@
+(function($) {
+    // Place any jQuery/helper plugins in here.
+
+    $(document).ready(function(){   
+        // Place code to run after page loads here.
+        
+    });
+}(jQuery));
+
